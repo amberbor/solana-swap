2a028ef800cb42136ea4b6376ef6285eb85540f4
Amber <amber.borici@y42.com>
2024-05-21 22:26:56 +0200

    string telegram

--- src/entity/test.py
+++ src/entity/test.py
@@ -1,43 +1,25 @@
-import asyncio
-import re
-import time
-from bs4 import BeautifulSoup
-from pyppeteer import launch
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
 
-start_time = time.time()
+api_id = 25926132
+api_hash = '784d85f68b93c3697824bacb37a7303b'
 
-async def fetch_page_source(url):
-    browser = await launch(headless=True)
-    page = await browser.newPage()
-    await page.goto(url)
-    # Wait for JavaScript to render (increase waiting time)
-    # await page.waitForSelector('body', timeout=5000)
-    await asyncio.sleep(1)  # Adjust the delay time as needed
-    content = await page.content()
-    await browser.close()
-    return content
+# Your session name
+session_name = 'testime'
+
+client = TelegramClient(session_name, api_id, api_hash)
 
 async def main():
-    # URL of the website
-    url = "https://rugcheck.xyz/tokens/DXcdWQp9QpvjSupD7hnX7BQxKKWsCaKXsaAxd9SFN3VM"
+    await client.start()
 
-    # Fetch the page source asynchronously
-    page_source = await fetch_page_source(url)
+    # Get the session string
+    session_string = StringSession.save(client.session)
+    print("Session String:", session_string)
 
-    # Parse the HTML content
-    soup = BeautifulSoup(page_source, 'html.parser')
+    print("Client connected successfully!")
+    me = await client.get_me()
+    print("My account:", me)
 
-    # Find all elements containing float percentage values (e.g., 0.00%)
-    percentage_elements = soup.find_all(string=re.compile(r'\b\d+\.\d+%'))
-
-    # Exclude the first two percentage elements
-    percentage_list = percentage_elements[3:]
-
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print("Execution time:", execution_time, "seconds")
-
-    print(percentage_list)
-
-# Run the event loop
-asyncio.get_event_loop().run_until_complete(main())
+# Run the main function
+with client:
+    client.loop.run_until_complete(main())
