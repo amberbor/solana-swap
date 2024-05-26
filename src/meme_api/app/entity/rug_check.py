d83c1ffc20ea73fbf55eb8470e3ddf1499ee58da
sbstxnext <boricisehar@gmail.com>
2024-05-26 23:41:22 +0200

    'last mod'

--- src/meme_api/app/entity/rug_check.py
+++ src/meme_api/app/entity/rug_check.py
@@ -4,7 +4,7 @@ import logging
 logger_2 = logging.getLogger(__name__)
 logger_2.setLevel(logging.INFO)
 
-file_handler = logging.FileHandler("app.log", mode="w")
+file_handler = logging.FileHandler("risks.log", mode="w")
 console_handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(formatter)
